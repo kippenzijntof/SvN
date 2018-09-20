ec86deda23096ef86876c9df35ddd30bf37f40ed
Roberto van Nugteren <rvnugteren@itpartner.nl>
2018-09-20 13:07:04 +0200

    Added some info as comments for the ServeiceExtension class in the Static project

--- Static/Init.cs
+++ Static/Init.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Static
 {
+    //This class is used by the UI project and its logic depends on the UI project requirements
     public static class ServiceExtension
     {
         public static void AddTransientBinding(this IServiceCollection serviceCollection)
