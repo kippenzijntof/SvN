c9737635b1b9548c331a9b6f5a9d43778ad39fa3
Roberto van Nugteren <rvnugteren@itpartner.nl>
2018-09-20 13:25:11 +0200

    Add comments to the AddTransientBinding method in the ServciceExtensions class

--- Static/Init.cs
+++ Static/Init.cs
@@ -8,6 +8,7 @@ namespace Static
     //This class is used by the UI project and its logic depends on the UI project requirements
     public static class ServiceExtension
     {
+        //This method sets up the bindings depending on the UI project requirements
         public static void AddTransientBinding(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<IProductRepository, ProductRepository>();
