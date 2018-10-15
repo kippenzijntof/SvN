3ab3411659b5a353d138ba223950d588c334ed85
RobertoFromHome <RobertoFromHome@example.com>
2018-10-16 01:17:39 +0200

    Try-out constructor params

--- ConcreteSVN/ProductRepository.cs
+++ ConcreteSVN/ProductRepository.cs
@@ -12,12 +12,20 @@ namespace ConcreteSVN
         {
             Context = context;
         }
-        public IQueryable<Product> Products => Context.Products.Select(x => new Product
+        //public IQueryable<Product> Products => Context.Products.Select(x => new Product()
+        //{
+        //    Id = x.Id,
+        //    Name = x.Name,
+        //    Description = x.Description,
+        //    Price = x.Price,
+        //    Category = x.Category
+        //});
+
+        //public IQueryable<Product> Products => Context.Products.Select(x => new Product(x.Id, x.Name, x.Description, x.Price, x.Category));
+        public IQueryable<Product> Products => Context.Products.Select(x => new Product(x.Id, x.Name)
         {
-            Id = x.Id,
-            Name = x.Name,
-            Description = x.Description,
-            Price = x.Price,
+            Description = x.Price > 100 ? $"20% DISCOUNT TODAY: {x.Description}" : x.Description,
+            Price = x.Price > 100 ? x.Price * (decimal)0.8 : x.Price,
             Category = x.Category
         });
     }
--- Interfaces/Product.cs
+++ Interfaces/Product.cs
@@ -4,10 +4,40 @@ namespace Interfaces
 {
     public class Product
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
+        //public Guid Id { get; set; }
+        //public string Name { get; set; }
+        //public string Description { get; set; }
+        //public decimal Price { get; set; }
+        //public string Category { get; set; }
+
+        //public Guid Id { get; }
+        //public string Name { get; }
+        //public string Description { get; }
+        //public decimal Price { get; }
+        //public string Category { get; }
+
+        //public Product(Guid id, string name, string description, decimal price, string category)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Description = description;
+        //    Price = price;
+        //    Category = category;
+        //}
+
+        public Guid Id { get; }
+        public string Name { get; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+
+        public Product(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        private Product()
+        { }
     }
 }
